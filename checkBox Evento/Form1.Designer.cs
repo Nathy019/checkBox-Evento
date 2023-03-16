00297e4327a6a3095243cc19f654c2249ccc7eeb
Nathalia Molina Da Silva <112526050+Nathy019@users.noreply.github.com>
2023-03-15 22:11:13 -0300

    Adicionar arquivos de projeto.

---
+++ checkBox Evento/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace checkBox_Evento
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBoxFla = new System.Windows.Forms.CheckBox();
+            this.checkBoxPal = new System.Windows.Forms.CheckBox();
+            this.checkBoxCor = new System.Windows.Forms.CheckBox();
+            this.checkBoxSao = new System.Windows.Forms.CheckBox();
+            this.checkBoxSan = new System.Windows.Forms.CheckBox();
+            this.checkBoxFlu = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // checkBoxFla
+            // 
+            this.checkBoxFla.AutoSize = true;
+            this.checkBoxFla.BackColor = System.Drawing.Color.Red;
+            this.checkBoxFla.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBoxFla.Location = new System.Drawing.Point(20, 62);
+            this.checkBoxFla.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.checkBoxFla.Name = "checkBoxFla";
+            this.checkBoxFla.Size = new System.Drawing.Size(94, 23);
+            this.checkBoxFla.TabIndex = 1;
+            this.checkBoxFla.Text = "Flamengo";
+            this.checkBoxFla.UseVisualStyleBackColor = false;
+            // 
+            // checkBoxPal
+            // 
+            this.checkBoxPal.AutoSize = true;
+            this.checkBoxPal.BackColor = System.Drawing.Color.Green;
+            this.checkBoxPal.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBoxPal.Location = new System.Drawing.Point(20, 92);
+            this.checkBoxPal.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.checkBoxPal.Name = "checkBoxPal";
+            this.checkBoxPal.Size = new System.Drawing.Size(94, 23);
+            this.checkBoxPal.TabIndex = 2;
+            this.checkBoxPal.Text = "Palmeiras";
+            this.checkBoxPal.UseVisualStyleBackColor = false;
+            // 
+            // checkBoxCor
+            // 
+            this.checkBoxCor.AutoSize = true;
+            this.checkBoxCor.BackColor = System.Drawing.Color.Black;
+            this.checkBoxCor.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold);
+            this.checkBoxCor.ForeColor = System.Drawing.Color.White;
+            this.checkBoxCor.Location = new System.Drawing.Point(20, 120);
+            this.checkBoxCor.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.checkBoxCor.Name = "checkBoxCor";
+            this.checkBoxCor.Size = new System.Drawing.Size(103, 23);
+            this.checkBoxCor.TabIndex = 3;
+            this.checkBoxCor.Text = "Corinthians";
+            this.checkBoxCor.UseVisualStyleBackColor = false;
+            // 
+            // checkBoxSao
+            // 
+            this.checkBoxSao.AutoSize = true;
+            this.checkBoxSao.BackColor = System.Drawing.Color.Red;
+            this.checkBoxSao.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold);
+            this.checkBoxSao.Location = new System.Drawing.Point(20, 149);
+            this.checkBoxSao.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.checkBoxSao.Name = "checkBoxSao";
+            this.checkBoxSao.Size = new System.Drawing.Size(95, 23);
+            this.checkBoxSao.TabIndex = 4;
+            this.checkBoxSao.Text = "São Paulo";
+            this.checkBoxSao.UseVisualStyleBackColor = false;
+            // 
+            // checkBoxSan
+            // 
+            this.checkBoxSan.AutoSize = true;
+            this.checkBoxSan.BackColor = System.Drawing.Color.Black;
+            this.checkBoxSan.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold);
+            this.checkBoxSan.ForeColor = System.Drawing.Color.White;
+            this.checkBoxSan.Location = new System.Drawing.Point(20, 178);
+            this.checkBoxSan.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.checkBoxSan.Name = "checkBoxSan";
+            this.checkBoxSan.Size = new System.Drawing.Size(72, 23);
+            this.checkBoxSan.TabIndex = 5;
+            this.checkBoxSan.Text = "Santos";
+            this.checkBoxSan.UseVisualStyleBackColor = false;
+            // 
+            // checkBoxFlu
+            // 
+            this.checkBoxFlu.AutoSize = true;
+            this.checkBoxFlu.BackColor = System.Drawing.Color.Green;
+            this.checkBoxFlu.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold);
+            this.checkBoxFlu.Location = new System.Drawing.Point(20, 206);
+            this.checkBoxFlu.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.checkBoxFlu.Name = "checkBoxFlu";
+            this.checkBoxFlu.Size = new System.Drawing.Size(102, 23);
+            this.checkBoxFlu.TabIndex = 6;
+            this.checkBoxFlu.Text = "Fluminense";
+            this.checkBoxFlu.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Location = new System.Drawing.Point(230, 64);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(278, 84);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Escolha o time ao lado  e clique aqui!";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Location = new System.Drawing.Point(385, 206);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(88, 26);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Sair";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(518, 274);
+            this.ControlBox = false;
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBoxFlu);
+            this.Controls.Add(this.checkBoxSan);
+            this.Controls.Add(this.checkBoxSao);
+            this.Controls.Add(this.checkBoxCor);
+            this.Controls.Add(this.checkBoxPal);
+            this.Controls.Add(this.checkBoxFla);
+            this.Font = new System.Drawing.Font("Microsoft JhengHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Escolha um Time";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBoxFla;
+        private System.Windows.Forms.CheckBox checkBoxPal;
+        private System.Windows.Forms.CheckBox checkBoxCor;
+        private System.Windows.Forms.CheckBox checkBoxSao;
+        private System.Windows.Forms.CheckBox checkBoxSan;
+        private System.Windows.Forms.CheckBox checkBoxFlu;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ checkBox Evento/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace checkBox_Evento
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string txt = "";
+
+            if (checkBoxFla.Checked)
+            {
+                txt += checkBoxFla.Text + ",";
+             }
+
+            if (checkBoxPal.Checked) 
+            {
+                txt += checkBoxPal.Text + ",";
+            }
+
+            if (checkBoxCor.Checked)
+            {
+                txt += checkBoxCor.Text + ",";
+            }
+
+            if (checkBoxSao.Checked)
+            {
+                txt += checkBoxSao.Text + ",";
+            }
+
+            if (checkBoxSan.Checked) 
+            {
+                txt += checkBoxSan.Text + ",";
+            }
+            if (checkBoxFlu.Checked)
+            {
+            txt = checkBoxFlu.Text + ",";
+            }
+            MessageBox.Show("O Time escolhido foi: " + txt, "O Time escolhido...");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
